0b862a2007e911d99211180180a1ef54ca8250ab
Sersasj <sasjsergioalvarezjunior@gmail.com>
2022-10-07 14:30:42 -0300

    ibagens

--- trab.py
+++ trab.py
@@ -1,32 +1,49 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  3 11:53:30 2022
 
-@author: sergi
-"""
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import cv2 as cv
 
-def limiarizacao(img, t: int, acima: bool, brilho: int):
-    if acima:
-        x = img >= t
-    else:
-        x = img < t
 
-    img = img.astype(int)
+def limiarizacao(img: np.ndarray, t: int, acima: bool, brilho: int) -> None:
+  if acima:
+    x = img >= t
+  else:
+    x = img < t
+  img = img.astype(int)
+  if brilho < 0:
+    img[x] = np.maximum(img[x] + brilho, 0)
+  else:
+    img[x] = np.minimum(img[x] + brilho, 255)
+  img = img.astype(np.uint8)
 
-    if brilho < 0:
-        img[x] = np.maximum(img[x]+brilho, 0)
-    else:
-        img[x] = np.minimum(img[x]+brilho, 255)
-
-    # agora damos cast de volta pra uint8
-    img = img.astype(np.uint8)
-
-    cv.imshow("teste", img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    cv.waitKey(1)
+  cv.imshow("teste", img)
+  cv.waitKey(0)
+  cv.destroyAllWindows()
+  cv.waitKey(1)
 
 
-limiarizacao(cv.imread("coelho.jpg",cv.IMREAD_GRAYSCALE), 200, False, -255)
+def main():
+  print("#--------------------------------------------------------------------------------------------#")
+  print("# Este programa recebe uma imagem em tons de cinza e realiza nesta uma alteração localizada  #\n"+ 
+    "# de brilho em função de uma operação de limiarização (thresholding)                         #")
+  print("#--------------------------------------------------------------------------------------------#")
+
+  #imagem = int(input("1 - Coelho\n2 - Cachorro\n3 - Gato\n"))
+  #if imagem == 1:
+  #    imagem = "coelho.jpg"
+  #elif imagem == 2:
+  #    imagem = "cachorro.jpg"    
+  #elif imagem == 3:
+  #    imagem = "gato.jpg"          
+  #t = int(input("Digite um valor de limiar"))
+  #brilho = int(input("Digite um valor de brilho "))
+  #acima = bool(input("Digite False ou True "))
+  print("#--------------------------------------------------------------------------------------------#")
+
+  limiarizacao(cv.imread("gato.jpg", cv.IMREAD_GRAYSCALE), 100, True, 180)
+
+
+if __name__ == "__main__":
+  main()
