5eae3907782f46da40140d135cba4042a3851fc2
stanyzra <stanyhelberth@gmail.com>
2022-11-03 16:46:22 -0300

    comentários e mudança na foto

--- trab2.py
+++ trab2.py
@@ -3,18 +3,29 @@ import cv2 as cv
 
 
 def invertHue(h, m, x):
+    # faz o cálculo dos limites superiores e inferiores
+    # com base em 180 por limitações da bibilioteca cv2
     upper = np.mod((m + x)/2, 180)
     lower = np.mod((m - x)/2, 180)
 
+    # cria matrizes booleanas de acordo com as condições
+    # de começo e fim do círculo HSV delimitado pelo
+    # usuário 
     start = h <= upper
     end = h >= lower
 
+    # realiza o casting para o tipo np.uint16
     h = h.astype(np.uint16)
 
+    # delimita qual operação será usada dependendo da 
+    # condição de que o limite inferior é menor do que 
+    # o limite superior 
     operation = start & end if lower < upper else start | end
 
+    # realiaza a operação (podendo ser união ou interseção)
     h[operation] = np.mod(h[operation] + 90, 180).astype(np.uint8)
 
+    # retorna o valor de hue castado para o tipo np.uint8 de volta
     return h.astype(np.uint8)
 
 
@@ -36,10 +47,12 @@ def hueAlteration(img: np.ndarray, m: int, x: int) -> None:
     hue, saturation, value = cv.split(image_hsv)
     # chama a função inverHue para inverter o valor de hue
     hue = invertHue(hue, m, x)
-
+    # após realizar as modificações, realiza o merge na imagem
     image_hsv = cv.merge([hue, saturation, value])
+    # converte a imagem de volta para RGB
     rgb = cv.cvtColor(image_hsv, cv.COLOR_HSV2BGR)
 
+    # mostra a imagem
     cv.imshow('Imagem Alterada', rgb)
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -47,7 +60,7 @@ def hueAlteration(img: np.ndarray, m: int, x: int) -> None:
 
 
 def main():
-    a = cv.imread('unknown.png')
+    a = cv.imread('coelho.jpg')
     m = int(input("Matiz: "))
     x = int(input("X: "))
 
